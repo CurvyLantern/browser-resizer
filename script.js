eb8c7729a18161d8daf07d725db0fcc37ab2d1f6
anaseem <ashfaqnaseem1@gmail.com>
2025-01-18 19:15:46 +0600

    bug fix

--- script.js
+++ script.js
@@ -6,20 +6,20 @@ const myForm = getElm('my_form')
 const myButton = getElm('my_button')
 
 
-let windowOpened = false
+
 let wp = null
 
 myForm.addEventListener('submit', event => {
   event.preventDefault()
 
-  if (windowOpened) {
+  if (wp) {
     wp.close()
+    myButton.textContent = 'Open'
+    wp = null
   }
   else {
     const fd = new FormData(event.target)
-
     const myUrl = fd.get('my_url')
-
     if (myUrl) {
       wp = window.open(myUrl, '', 'width=320')
       myButton.textContent = 'Close'
